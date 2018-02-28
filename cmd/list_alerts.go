8ef915397875163bdd6c068686d2ff9eddcff543
Kevin Elledge <kelledge@hurricanesi.com>
2018-02-27 22:32:45 -0600

    Custom JSON Unmarashal for dates; Sort dates; Simple polling
    Most of these are just proof-of-concepts for concepts I am reasonably sure will
    be needed in the completed script.
    
    Namely:
      1) Interpreting JSON data: This simplifies most other tasks working with the
         data
      2) Sorting date fields: Polling this data will require paging both over time
         and server responses.
      3) Polling: A worker will need to regularly poll the API for new data.
    
    A deduper will also be required to ensure that only new alerts/events are
    passed through the processing pipeline. This will likely need to track a list
    of alert SIDs that have already been "seen". Keeping run state minimal will be
    a primary focus for this component.

--- cmd/list_alerts.go
+++ cmd/list_alerts.go
@@ -1,12 +1,22 @@
 package main
 
 import (
-	"github.com/kelledge/serviceTails/twilio"
 	"fmt"
+	"os"
+
+	"github.com/kelledge/serviceTails/twilio"
 )
 
 func main() {
-	monitor := twilio.NewTwilioMonitorClient("ACabfe3927adeb3ccdde96c4ff19169620", "2c3d1dd51fa054bae8a6e6c64f0a4b94")
-	list, _ := monitor.List("2018-02-16T20:43:17Z", "2018-02-16T21:01:27Z")
-	fmt.Printf("%s", list)
+	accountSid := os.Getenv("TWILIO_ACCOUNT_SID")
+	authToken := os.Getenv("TWILIO_AUTH_TOKEN")
+	monitor := twilio.NewTwilioMonitorClient(accountSid, authToken)
+
+	poller := monitor.Poll()
+
+	for result := range poller {
+		for _, alert := range result {
+			fmt.Printf("%v\n", alert)
+		}
+	}
 }
--- twilio/monitor_client.go
+++ twilio/monitor_client.go
@@ -1,28 +1,76 @@
 package twilio
 
-import "net/http"
-import "net/url"
-import "encoding/json"
+import (
+	"encoding/json"
+	"net/http"
+	"sort"
+	"time"
+)
+
+const Error = "error"
+const Warning = "warning"
+const Notice = "notice"
+const Debug = "debug"
+
+type Interval struct {
+	Start time.Time
+	End   time.Time
+}
+
+type MonitorDate struct {
+	time.Time
+}
+
+func (date *MonitorDate) UnmarshalJSON(data []byte) error {
+	var iso8601 string
+	json.Unmarshal(data, &iso8601)
+	date.Time, _ = time.Parse(time.RFC3339, iso8601)
+	return nil
+}
 
 type MonitorAlert struct {
-	Sid string `json:"sid"`
-	AccountSid string `json:"account_sid"`
-	ServiceSid string `json:"service_sid"`
-	LogLevel   string `json:"log_level"`
-	ErrorCode string `json:"error_code"`
-	DateUpdate string `json:"date_updated"`
-	DateGenerated string `json:"date_generated"`
-	Url string `json:"url"`
+	AlertText        string `json:"alert_text"`
+	RequestURL       string `json:"request_url"`
+	RequestMethod    string `json:"request_method"`
+	RequestVariables string `json:"request_variables"`
+	ResponseHeaders  string `json:"response_headers"`
+	ResponseBody     string `json:"response_body"`
+}
+
+type MonitorTruncatedAlert struct {
+	Sid           string      `json:"sid"`
+	AccountSid    string      `json:"account_sid"`
+	ServiceSid    string      `json:"service_sid"`
+	LogLevel      string      `json:"log_level"`
+	ErrorCode     string      `json:"error_code"`
+	DateUpdate    MonitorDate `json:"date_updated"`
+	DateGenerated MonitorDate `json:"date_generated"`
+	URL           string      `json:"url"`
 }
 
 type MonitorAlertListMeta struct {
-	Page int `json:"page"`
+	Page     int `json:"page"`
 	PageSize int `json:"page_size"`
 }
 
 type MonitorAlertList struct {
-	Alerts []*MonitorAlert `json:"alerts"`
-	Meta *MonitorAlertListMeta `json:"meta"`
+	Alerts []*MonitorTruncatedAlert `json:"alerts"`
+	Meta   *MonitorAlertListMeta    `json:"meta"`
+}
+
+func (list *MonitorAlertList) Sort() *MonitorTruncatedAlert {
+	sort.Slice(list.Alerts, func(i, j int) bool {
+		return list.Alerts[i].DateGenerated.Unix() < list.Alerts[j].DateGenerated.Unix()
+	})
+	return nil
+}
+
+func (list *MonitorAlertList) Interval() *Interval {
+	lastIndex := len(list.Alerts) - 1
+	return &Interval{
+		Start: list.Alerts[0].DateGenerated.Time,
+		End:   list.Alerts[lastIndex].DateGenerated.Time,
+	}
 }
 
 type MonitorClient struct {
@@ -30,25 +78,51 @@ type MonitorClient struct {
 	AuthToken  string
 	BaseUrl    string
 	HttpClient *http.Client
+	// NOTE: Belongs on separate unit
+	Ticker *time.Ticker
 }
 
 func NewTwilioMonitorClient(accountSid, authToken string) *MonitorClient {
 	return &MonitorClient{
-		AccountSid:accountSid,
-		AuthToken:authToken,
-		BaseUrl: "https://monitor.twilio.com/v1/Alerts/",
+		AccountSid: accountSid,
+		AuthToken:  authToken,
+		BaseUrl:    "https://monitor.twilio.com/v1/Alerts/",
 		HttpClient: &http.Client{},
 	}
 }
 
-func (twilio *MonitorClient) List(startDate, endDate string) (*MonitorAlertList, error) {
-	url := url.UR
+func (twilio *MonitorClient) Do(req *http.Request) (*http.Response, error) {
+	req.SetBasicAuth(twilio.AccountSid, twilio.AuthToken)
+	return twilio.HttpClient.Do(req)
+}
+
+func (twilio *MonitorClient) List(startDate, endDate, logLevel string) (*MonitorAlertList, error) {
 	req, _ := http.NewRequest("GET", twilio.BaseUrl, nil)
-	res, _ := twilio.HttpClient.Do(req)
+
+	q := req.URL.Query()
+	q.Add("LogLevel", logLevel)
+	q.Add("StartDate", startDate)
+	q.Add("EndDate", endDate)
+
+	res, _ := twilio.Do(req)
 
 	decoder := json.NewDecoder(res.Body)
 	var list MonitorAlertList
 	decoder.Decode(&list)
 
 	return &list, nil
+}
+
+// NOTE: Belongs on separate unit
+func (twilio *MonitorClient) Poll() <-chan []*MonitorTruncatedAlert {
+	twilio.Ticker = time.NewTicker(10 * time.Second)
+	responses := make(chan []*MonitorTruncatedAlert)
+	go func() {
+		for _ = range twilio.Ticker.C {
+			res, _ := twilio.List("2018-02-16T20:43:17Z", "2018-02-16T21:01:27Z", Error)
+			responses <- res.Alerts
+		}
+	}()
+
+	return responses
 }
